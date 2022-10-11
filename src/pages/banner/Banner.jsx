4fd3ce7c75604ba3aa5a455231ba31979823ba6e
pranta <prantaacharjee647@gmail.com>
2022-10-11 12:11:40 +0600

    start banner

--- src/pages/banner/Banner.jsx
+++ src/pages/banner/Banner.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='container'>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div>
+         <h3>Your own home to organize<br/> just everything.</h3>
+         <p>Turn chaos into productivity, organize everything that you can think of into beautiful home pages and become more focused.</p>
+         <button>Try Now, It’s Free!</button>
+         <p><small>No credit card required.</small></p>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
